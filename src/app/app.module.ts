3c87c71821465c3b82fc3840efe315fa133555a8
Abhiraaga <abhijeet.gaikwad75@yahoo.com>
2019-02-13 12:27:59 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule,MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
+import { TablecomponentComponent } from './tablecomponent/tablecomponent.component';
+import {MatTableModule} from '@angular/material/table';
+import {CdkTableModule} from '@angular/cdk/table';
+import { Table1Component } from './table1/table1.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablecomponentComponent,
+    Table1Component
   ],
   imports: [
+    CdkTableModule,
+    MatTableModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCheckboxModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule
   ],
-  providers: [],
+  providers: [MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
